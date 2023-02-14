98f49946ea46b36273af821c4b2607bff09857e1
Julia <juliarusova2014@gmail.com>
2023-02-14 14:15:33 +0200

    Downloaded empty project

---
+++ Game "Catch me if you can"/Game "Catch me if you can"/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Game "Catch me if you can"
+//
+//  Created by Julia on 13.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
