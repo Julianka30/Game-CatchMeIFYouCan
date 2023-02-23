8a17b0f99de5127ba6ad7c0bb145fac8b791f667
Julia <juliarusova2014@gmail.com>
2023-02-23 11:32:51 +0200

    Fixes after review

--- Game "Catch me if you can"/Game "Catch me if you can"/ViewController.swift
+++ Game "Catch me if you can"/Game "Catch me if you can"/ViewController.swift
@@ -8,22 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var count = 0
-    let secondsPerGame = 10
-    var secondsRemaining = 0
-    var myTimer = Timer()
+    private var count = 0
+    private let secondsPerGame = 10
+    private var secondsRemaining = 0
+    private var timer = Timer()
     
+    @IBOutlet private weak var playField: UIView!
+    @IBOutlet private weak var redSquare: UIButton!
+    @IBOutlet private weak var timerLabel: UILabel!
+    @IBOutlet private weak var switchLevel: UISegmentedControl!
     
-    @IBOutlet weak var playField: UIView!
-    
-    @IBOutlet weak var redSquare: UIButton!
-    
-    @IBOutlet weak var timer: UILabel!
-    
-    @IBOutlet weak var switchLevel: UISegmentedControl!
-    
-    
-    @IBAction func tapSquare(_ sender: UIButton) {
+    @IBAction private func tapSquare(_ sender: UIButton) {
         let maxX = playField.frame.size.width - redSquare.frame.size.width
         let maxY = playField.frame.size.height - redSquare.frame.size.height
         redSquare.frame.origin = CGPoint(x: .random(in: 0...maxX), y: .random(in: 0...maxY))
@@ -34,34 +29,35 @@ class ViewController: UIViewController {
         count += 1
     }
 
-    func runTimer() {
-        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+    private func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
     }
     
-    @objc func updateTimer() {
+    @objc
+    private func updateTimer() {
         if secondsRemaining > 0 {
             secondsRemaining -= 1
         } else {
-            myTimer.invalidate()
-            showAlert(count: count, time: secondsPerGame)
+            timer.invalidate()
+            showAlert()
             secondsRemaining = secondsPerGame
             count = 0
             redSquare.frame.origin = CGPoint(x: 8, y: 8)
         }
-        timer.text = timeString(time: TimeInterval(secondsRemaining))
+        timerLabel.text = formattedTime(time: TimeInterval(secondsRemaining))
     }
     
-    func timeString(time: TimeInterval) -> String {
+    private func formattedTime(time: TimeInterval) -> String {
         let minutes = (Int(time) / 60) % 60
         let seconds = Int(time) % 60
         return String(format: "%02i:%02i", minutes, seconds)
     }
     
-    func showAlert(count: Int, time: Int) {
-        let alertController = UIAlertController(title: "", message: "You clicked \(count) times for \(time) seconds", preferredStyle: .alert)
-        let OKAction = UIAlertAction(title: "OK", style: .default)
-        alertController.addAction(OKAction)
-        self.present(alertController, animated: true)
+    private func showAlert() {
+        let alertController = UIAlertController(title: "", message: "You clicked \(count) times for \(secondsPerGame) seconds", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(okAction)
+        present(alertController, animated: true)
     }
 }
 
